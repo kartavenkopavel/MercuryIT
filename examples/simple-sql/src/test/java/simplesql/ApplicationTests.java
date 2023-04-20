bc11b535c7ce99c8cad6ab04d6b93bb31d57224c
Kartavenko Pavel <gregg190@gmail.com>
2023-04-20 07:36:08 +0300

    added tests: testEditEmployee(), testUpdateEmployee() pom added a dependency for use in the local repository test refactoring

--- examples/simple-sql/src/test/java/simplesql/ApplicationTests.java
+++ examples/simple-sql/src/test/java/simplesql/ApplicationTests.java
@@ -18,14 +18,6 @@ public class ApplicationTests {
     @LocalServerPort
     private int port;
 
-    private String getUrl(String endPoint, Object... args) {
-        return String.format("http://localhost:%d/api/employees/%s", port, String.format(endPoint, args));
-    }
-
-    private String getUrl(Long id) {
-        return getUrl(id.toString());
-    }
-
     private final EmployeeEntity storedEmployee = EmployeeEntity.builder()
             .name("Pavel")
             .title("QA")
@@ -35,7 +27,7 @@ public class ApplicationTests {
     @Order(1)
     public void testCreateEmployee() {
         MercuryIT.request(MercuryITHttp.class)
-                .uri(getUrl("create"))
+                .uri(String.format("http://localhost:%d/api/employees/create", port))
                 .body(storedEmployee)
                 .post()
                 .assertion(MercuryITHttpResponse::getCode).equalsTo(200)
@@ -50,7 +42,7 @@ public class ApplicationTests {
     @Order(2)
     public void testGetListEmployees() {
         MercuryIT.request(MercuryITHttp.class)
-                .uri(getUrl("list"))
+                .uri(String.format("http://localhost:%d/api/employees/list", port))
                 .get()
                 .assertion(MercuryITHttpResponse::getCode).equalsTo(200)
                 .apply(response ->
@@ -63,21 +55,59 @@ public class ApplicationTests {
     @Order(3)
     public void testGetEmployeeById() {
         MercuryIT.request(MercuryITHttp.class)
-                .uri(getUrl(storedEmployee.getId()))
+                .uri(String.format("http://localhost:%d/api/employees/%d", port, storedEmployee.getId()))
                 .get()
                 .assertion(MercuryITHttpResponse::getCode).equalsTo(200)
-                .apply(response -> {
-                    Assertions.assertEquals(storedEmployee, response.getBody(EmployeeEntity.class));
-                });
+                .apply(response ->
+                    Assertions.assertEquals(storedEmployee, response.getBody(EmployeeEntity.class))
+                );
     }
 
     @Test
     @Order(4)
+    public void testEditEmployee() {
+        EmployeeEntity editEmployee = EmployeeEntity.builder()
+                .id(storedEmployee.getId())
+                .name("Sergei")
+                .build();
+
+        MercuryIT.request(MercuryITHttp.class)
+                .uri(String.format("http://localhost:%d/api/employees/edit", port))
+                .body(editEmployee)
+                .put()
+                .assertion(MercuryITHttpResponse::getCode).equalsTo(200)
+                .apply(response -> {
+                    Assertions.assertNull(response.getBody(EmployeeEntity.class).getTitle());
+                    Assertions.assertEquals(editEmployee, response.getBody(EmployeeEntity.class));
+                });
+    }
+
+    @Test
+    @Order(5)
+    public void testUpdateEmployee() {
+        EmployeeEntity editEmployee = EmployeeEntity.builder()
+                .title("Developer")
+                .build();
+
+        MercuryIT.request(MercuryITHttp.class)
+                .uri(String.format("http://localhost:%d/api/employees/update/%d", port, storedEmployee.getId()))
+                .body(editEmployee)
+                .patch()
+                .assertion(MercuryITHttpResponse::getCode).equalsTo(200)
+                .apply(response -> {
+                    Assertions.assertNotNull(response.getBody(EmployeeEntity.class).getName());
+                    Assertions.assertEquals("Sergei",  response.getBody(EmployeeEntity.class).getName());
+                    Assertions.assertEquals(editEmployee.getTitle(), response.getBody(EmployeeEntity.class).getTitle());
+                });
+    }
+
+    @Test
+    @Order(6)
     public void testDeleteEmployee() {
         MercuryIT.request(MercuryITHttp.class)
-                .uri(getUrl("delete/%d", storedEmployee.getId()))
+                .uri(String.format("http://localhost:%d/api/employees/delete/%d", port, storedEmployee.getId()))
                 .delete()
                 .assertion(MercuryITHttpResponse::getCode).equalsTo(200)
-                .assertion(MercuryITHttpResponse::getBody).equalsTo("");
+                .assertion(MercuryITHttpResponse::getBody).isEmpty();
     }
 }
